de3aeb5407bd042e3acf73c9ce811736e9b935e3
AlexGyver <alexgyvershow@ya.ru>
2024-12-28 16:12:15 +0300

    Update FastLEDsupport.h

--- src/FastLEDsupport.h
+++ src/FastLEDsupport.h
@@ -5,5 +5,13 @@
 #include <FastLED.h>
 #include "microLED.h"
 
-mData CRGBtoData(CRGB col);
+static mData CRGBtoData(CRGB col) {
+#if (COLOR_DEBTH == 1)
+    return RGBto8(col.r, col.g, col.b);
+#elif (COLOR_DEBTH == 2)
+    return RGBto16(col.r, col.g, col.b);
+#elif (COLOR_DEBTH == 3)
+    return mData(col.r, col.g, col.b);
+#endif
+}
 #endif
